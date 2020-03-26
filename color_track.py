7261e300803b1fd59a38f6c7c4870cf8f2b82c2e
SuWeipeng <weipeng.su@outlook.com>
2020-03-26 10:30:51 +0800

    识别颜色斑点

--- color_track.py
+++ color_track.py
@@ -1,6 +1,8 @@
 import sensor
 import image
 
+red_threshold = (0, 80, 43, 75, 35, 55)
+
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QQVGA)
@@ -9,3 +11,6 @@ sensor.set_auto_whitebal(False)
 
 while(True):
   img = sensor.snapshot()
+  blobs = img.find_blobs([red_threshold])
+  if blobs:
+    print(blobs)
