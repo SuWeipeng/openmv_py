155f073c592596699fe13519541170529db36c50
SuWeipeng <weipeng.su@outlook.com>
2020-03-26 10:32:44 +0800

    打印每秒处理的帧数

--- color_track.py
+++ color_track.py
@@ -1,5 +1,6 @@
 import sensor
 import image
+import time
 
 red_threshold = (0, 80, 43, 75, 35, 55)
 
@@ -9,10 +10,14 @@ sensor.set_framesize(sensor.QQVGA)
 sensor.skip_frames(10)
 sensor.set_auto_whitebal(False)
 
+clock = time.clock()
+
 while(True):
+  clock.tick()
   img = sensor.snapshot()
   blobs = img.find_blobs([red_threshold])
   if blobs:
     print(blobs)
     for b in blobs:
       img.draw_rectangle(b[0:4])
+  print(clock.fps())
