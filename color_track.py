ba0f18d6c721eb85d8adeae5f8b3b6f963436c70
SuWeipeng <weipeng.su@outlook.com>
2020-03-26 10:29:50 +0800

    获取一帧图像

--- color_track.py
+++ color_track.py
@@ -1,7 +1,11 @@
 import sensor
+import image
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QQVGA)
 sensor.skip_frames(10)
 sensor.set_auto_whitebal(False)
+
+while(True):
+  img = sensor.snapshot()
